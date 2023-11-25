6aec553ee0e8648ef3759501930bab2e43e37aba
Kai Kosades <red4truck@tutanota.com>
2023-11-26 01:51:47 +0200

    Fixed build by adding vector unit tests

---
+++ matrixes/unit_tests/src/unittest_vector.cpp
@@ -0,0 +1,23 @@
+#include "unittest_common.h"
+
+#include "vector.h"
+
+using namespace MxLib;
+using namespace MxLib::algo;
+
+TEST(VectorConstructionTest, VectorDefaultConstructorTests)
+{
+	MxLib::VectorI vec{};
+	ASSERT_EQ(vec.size(), 0);
+}
+TEST(VectorConstructionTest, VectorBasicConstructorTests)
+{
+	MxLib::VectorI vec{1, 2, 3, 4, 5};
+	ASSERT_EQ(vec.size(), 5);
+
+	const std::array expected{1, 2, 3, 4, 5};
+	for (int i = 0; i < expected.size(); ++i)
+	{
+		ASSERT_EQ(vec[i], expected[i]);
+	}
+}
